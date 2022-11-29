24a840e72f7c9a329d71f8639a013605d1ced1fb
shaibal-tiller <114130885+shaibal-tiller@users.noreply.github.com>
2022-11-29 15:32:12 +0600

    Update pwabuilder-sw.js

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -74,3 +74,10 @@
 //     })());
 //   }
 // });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
